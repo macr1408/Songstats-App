9e4aada662775fc2a5b90a2028a36ca448bd3ecf
Marcos <macr1408@hotmail.com>
2020-03-01 02:39:27 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="flex justify-center items-center min-h-screen">
+      <div class="text-center">
+        <Logo class="block"></Logo>
+        <a
+          class="bg-green-spotify text-white font-bold mt-6 px-8 py-2 rounded-full inline-block"
+          href="http://localhost:8000/login"
+        >Iniciar sesión</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+    theme: {
+        container: {
+            center: true
+        },
+        extend: {
+            colors: {
+                'green-spotify': '#1db954',
+                'gray-spotify': '#222326'
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
