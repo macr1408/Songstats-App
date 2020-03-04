95daa5b89573edc7622d7cf7c34fa5cc3dd1bd4a
Marcos <macr1408@hotmail.com>
2020-03-04 09:09:44 -0300

    Fix login screen

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,13 @@
         <Logo class="block"></Logo>
         <a
           class="bg-green-spotify text-white font-bold mt-6 px-8 py-2 rounded-full inline-block"
+          :class="this.isLoading ? 'spinner' : ''"
           href="http://localhost:8000/login"
         >Iniciar sesión</a>
+        <h2
+          class="text-red-500 mt-2"
+          v-if="this.loginError"
+        >Hubo un error, por favor intenta nuevamente</h2>
       </div>
     </div>
   </div>
@@ -14,10 +19,38 @@
 
 <script>
 import Logo from "~/components/Logo.vue";
+const axios = require("axios");
 
 export default {
+  data() {
+    return {
+      loginError: false,
+      isLoading: false
+    };
+  },
   components: {
     Logo
+  },
+  methods: {
+    login: function(code) {
+      this.isLoading = true;
+      axios
+        .post("http://localhost:8000/api/v1/auth", { code })
+        .then(response => {
+          this.loginError = false;
+          this.isLoading = false;
+        })
+        .catch(error => {
+          this.loginError = true;
+          this.isLoading = false;
+        });
+    }
+  },
+  mounted() {
+    const spotifyCode = this.$router.currentRoute.query.code;
+    if (spotifyCode) {
+      this.login(spotifyCode);
+    }
   }
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@
 */
 module.exports = {
     theme: {
+        spinner: (theme) => ({
+            default: {
+                color: '#dae1e7', // color you want to make the spinner
+                size: '1em', // size of the spinner (used for both width and height)
+                border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+                speed: '500ms', // the speed at which the spinner should rotate
+            }
+        }),
         container: {
             center: true
         },
@@ -16,6 +24,10 @@ module.exports = {
             }
         }
     },
-    variants: {},
-    plugins: []
+    variants: {
+        spinner: ['responsive']
+    },
+    plugins: [
+        require('tailwindcss-spinner')()
+    ]
 }
